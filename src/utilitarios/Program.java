9bade845413eb3338968193af5b226c6dd69f18f
mscrodrigo <mscrodrigo@gmail.com>
2018-12-03 18:03:04 -0200

    Mensagem boa noite

--- src/utilitarios/Program.java
+++ src/utilitarios/Program.java
@@ -5,7 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Boa tarde");
-
+		System.out.println("Boa noite");
 	}
-
 }
